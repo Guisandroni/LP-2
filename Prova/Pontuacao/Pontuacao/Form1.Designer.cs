b1f8431e6529adbb3f7bb327362f93861d6587ee
Guilherme Sandroni Dias <89549237+Guisandroni@users.noreply.github.com>
2023-05-16 11:05:44 -0300

    Add files via upload

---
+++ Prova/Pontuacao/Pontuacao/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Pontuacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExec = new System.Windows.Forms.Button();
+            this.lbxsaida = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(170, 71);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(206, 168);
+            this.btnExec.TabIndex = 0;
+            this.btnExec.Text = "Executar";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lbxsaida
+            // 
+            this.lbxsaida.FormattingEnabled = true;
+            this.lbxsaida.Location = new System.Drawing.Point(462, 56);
+            this.lbxsaida.Name = "lbxsaida";
+            this.lbxsaida.Size = new System.Drawing.Size(298, 251);
+            this.lbxsaida.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbxsaida);
+            this.Controls.Add(this.btnExec);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.ListBox lbxsaida;
+    }
+}
+
---
+++ Prova/Pontuacao/Pontuacao/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace Pontuacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[,] matriz = new int[11, 3];
+
+            string entrada = " ";
+            int media, jogadores, pontos;
+
+
+            for (var i = 0; i < 11; i++)
+
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    entrada = Interaction.InputBox("Digite o numero jogador " + (i + 1), "Entrada dos pontos" + (j + 1));
+                    if (!int.TryParse(entrada, out matriz[i, j]) || matriz[i, j] < 0)
+                    {
+                        MessageBox.Show("digite um valor inteiro não negativo");
+
+                    }
+                    media = matriz[i, j] / 3;
+                    pontos = matriz[i, j];
+
+
+
+
+                }
+            }
+            lbxsaida.Items.Add("Jogador 1: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 2: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 3: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 4: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 5: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 6: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+            lbxsaida.Items.Add("Jogador 7: " + "Pontos e Nivel: " + "Pontos e Nivel" + "Pontos e Nivel" + "Media: ");
+
+
+
+        }
+    } }
